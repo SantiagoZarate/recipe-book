da957cb8ca1ac3b3a690e8477f387cd3e3ca667b
SantiagoZarate <santiago2001zarate@gmail.com>
2025-01-19 21:18:02 -0300

    chore: add npmrn to install local packages

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,7 @@ export default [
       // note you must disable the base rule
       // as it can report incorrect errors
       "no-unused-vars": "off",
+      "@typescript-eslint/no-require-imports": "off",
       "@typescript-eslint/no-unused-vars": [
         "warn", // or "error"
         {
