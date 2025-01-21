97a3c138fbe28723230e165a911901c32d8a1fc5
SantiagoZarate <santiago2001zarate@gmail.com>
2025-01-21 14:44:21 -0300

    chore: trying to add mobile and web repos to git historty

--- packages/api-client/api/dish/dish.api.ts
+++ packages/api-client/api/dish/dish.api.ts
@@ -1,27 +1,28 @@
-import { baseUrl } from "../..";
 import type { DishAPI } from "../../types/dish";
 
-const dishApi: DishAPI = {
-  getAll() {
-    const url = baseUrl + "/dish";
+function dishApi(baseUrl: string): DishAPI {
+  return {
+    getAll() {
+      const url = baseUrl + "/dish";
 
-    return fetch(url).then((response) => {
-      if (!response.ok) {
-        throw new Error("Error getting all the dishes");
-      }
-      return response.json();
-    });
-  },
-  getOne({ id }) {
-    const url = `${baseUrl}/dish/${id}`;
+      return fetch(url).then((response) => {
+        if (!response.ok) {
+          throw new Error("Error getting all the dishes");
+        }
+        return response.json();
+      });
+    },
+    getOne({ id }) {
+      const url = `${baseUrl}/dish/${id}`;
 
-    return fetch(url).then((response) => {
-      if (!response.ok) {
-        throw new Error(`Error getting dish with id ${id}`);
-      }
-      return response.json();
-    });
-  },
-};
+      return fetch(url).then((response) => {
+        if (!response.ok) {
+          throw new Error(`Error getting dish with id ${id}`);
+        }
+        return response.json();
+      });
+    },
+  };
+}
 
 export { dishApi };
--- packages/api-client/index.ts
+++ packages/api-client/index.ts
@@ -1,11 +1,7 @@
 import { dishApi } from "./api/dish/dish.api";
 
-// export const baseUrl = "http://localhost:3000/api";
-// export const baseUrl = "http://10.0.2.2:3000/api";
-export const baseUrl = "http://192.168.0.9:3000/api";
-
-const api = {
-  dish: dishApi,
-};
+const api = (baseUrl: string) => ({
+  dish: dishApi(baseUrl),
+});
 
 export { api };
